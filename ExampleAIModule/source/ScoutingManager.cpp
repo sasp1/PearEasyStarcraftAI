35e651bb7d478840901af69a68e57e4146ffcfff
DESKTOP-9NMV119\Asger <s154099@student.dtu.dk>
2018-03-14 14:12:22 +0100

    scout

--- ExampleAIModule/source/ScoutingManager.cpp
+++ ExampleAIModule/source/ScoutingManager.cpp
@@ -15,7 +15,19 @@ ScoutingManager::~ScoutingManager()
 }
 
 void ScoutingManager::addScout(const BWAPI::Unit* scout) {
-	//Receive control of a new worker
+	//Receive control of a new scoutUnit
 	scoutingUnits.push_back(scout);
+	
 }
 
+void ScoutingManager::moveScoutToTile(const BWAPI::Unit* scout, int x, int y) {
+	//Moves unit to given tile in given tilescale
+	(*scout)->move(Position(x, y));
+}
+
+void ScoutingManager::executeOrders() {
+	int size = scoutingUnits.size(); 
+	if (size==1) {
+		moveScoutToTile(scoutingUnits.back(), 4000, 0);
+	}
+}
--- ExampleAIModule/source/ScoutingManager.h
+++ ExampleAIModule/source/ScoutingManager.h
@@ -3,8 +3,11 @@
 class ScoutingManager
 {
 public:
+	std::list<const BWAPI::Unit*> scoutingUnits;
 	ScoutingManager();
 	~ScoutingManager();
 	void ScoutingManager::addScout(const BWAPI::Unit* scout);
+	void ScoutingManager::moveScoutToTile(const BWAPI::Unit* scout, int tileNr, int tileScale);
+	void ScoutingManager::executeOrders();
 };
 
