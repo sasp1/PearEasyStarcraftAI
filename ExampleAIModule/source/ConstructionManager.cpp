974f4bf5d13bf0496bcef2eb437a473290ba6278
s164394 <s164394@student.dtu.dk>
2018-04-04 14:58:54 +0200

    heeeeeeej

--- ExampleAIModule/source/ConstructionManager.cpp
+++ ExampleAIModule/source/ConstructionManager.cpp
@@ -102,7 +102,7 @@ void ConstructionManager::buildRefinery(const BWAPI::Unit* worker) {
 
 	if (distance != 10000) {
 
-		Broodwar->sendText("Refinery");
+		// Broodwar->sendText("Refinery");
 
 		BWAPI::Position pos = (*gasLocation)->getPosition();
 
