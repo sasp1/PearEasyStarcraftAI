39b19267e805f5de2662bd923116638f88270434
s164394 <s164394@student.dtu.dk>
2018-04-04 16:58:42 +0200

    Heeeeej

--- ExampleAIModule/source/ExampleAIModule.cpp
+++ ExampleAIModule/source/ExampleAIModule.cpp
@@ -88,10 +88,10 @@ void ExampleAIModule::onUnitComplete(BWAPI::Unit unit)
 {//When a unit build is complete
 	BWAPI::Unit* u = new Unit(unit);
 
-	//Make sure we are ingame
-	if (Broodwar->getFrameCount() > 10) {
+	//Make sure we are ingame 
+	// makes sure unit is ally unit
+	if (Broodwar->self()->isAlly((*u)->getPlayer()) && Broodwar->getFrameCount() > 10) {
 
-		
 
 		if ((*u)->getType().isWorker()) 
 			(*unitManager).newWorker(u);
