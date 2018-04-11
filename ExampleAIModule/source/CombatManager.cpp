32003f76af8ecb848647257d3ca2411434744443
DESKTOP-9NMV119\Asger <s154099@student.dtu.dk>
2018-04-11 17:19:36 +0200

    lol

--- ExampleAIModule/source/CombatManager.cpp
+++ ExampleAIModule/source/CombatManager.cpp
@@ -10,19 +10,38 @@ CombatManager::CombatManager() {
 CombatManager::~CombatManager() {
 }
 
+
 void CombatManager::addCombatUnit(const BWAPI::Unit* unit) {
 	//Receive control of a new combatUnit
 	combatUnits.push_back(unit);
 }
 
 void CombatManager::attackNearestEnemy(const BWAPI::Unit* unit) {
-		(*unit)->attack((*unit)->getClosestUnit(IsEnemy), false);
+	(*unit)->attack((*unit)->getClosestUnit((IsEnemy && IsAttacking)));
+	(*unit)->attack((*unit)->getClosestUnit(IsEnemy));
+}
+
+void CombatManager::attackEnemy(const BWAPI::Unit* unit, const BWAPI::Unit target) {
+	(*unit)->attack(target);
+}
+
+
+void CombatManager::attackEnemyIfInRange(const BWAPI::Unit* unit, BWAPI::UnitType target, int range) {
+	for (auto &eu : (Broodwar->enemy()->getUnits())) {
+		if (eu->getType() == target && (*unit)->getDistance(eu) < range) {
+			(*unit)->attack(eu);
+		}
+	}
 }
 
 void CombatManager::attackEnemyBaseWithAllCombatUnits(BWAPI::Position pos) {
+	//const BWAPI::Unit* combatLeader = combatUnits.back();
+	//const BWAPI::Unit nearestEnemy = (*combatLeader)->getClosestUnit(IsEnemy);
+	
 	for (auto &u : combatUnits) {
-			attackNearestEnemy(u);
-			(*u)->move(pos);
+		attackEnemyIfInRange(u, UnitTypes::Terran_Marine, 10);
+		attackNearestEnemy(u);
+		(*u)->move(pos);
 	}
 }
 
--- ExampleAIModule/source/CombatManager.h
+++ ExampleAIModule/source/CombatManager.h
@@ -5,13 +5,15 @@ class CombatManager
 {
 public:
 	BuildingManager* buildingManager;
+
+
 	
 	std::list<const BWAPI::Unit*> combatUnits;
 
-
-	
 	void CombatManager::addCombatUnit(const BWAPI::Unit* unit);
 	void CombatManager::attackNearestEnemy(const BWAPI::Unit* unit);
+	void CombatManager::attackEnemy(const BWAPI::Unit* unit, const BWAPI::Unit target);
+	void CombatManager::attackEnemyIfInRange(const BWAPI::Unit* unit, BWAPI::UnitType target, int range);
 	void CombatManager::attackEnemyBaseWithAllCombatUnits(BWAPI::Position pos);
 	void CombatManager::defendBase();
 
