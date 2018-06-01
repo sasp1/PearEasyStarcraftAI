ea24ea51e3b9d8b2c422bdf1147f0feae470190c
Fjordhøj <fjordhoj@hotmail.com>
2018-06-01 12:45:57 +0200

    Build + construct update

--- ExampleAIModule/source/BuildingManager.h
+++ ExampleAIModule/source/BuildingManager.h
@@ -3,23 +3,19 @@
 class BuildingManager
 {
 
-
-	
 public:
 	BuildingManager();
 	~BuildingManager();
 	void BuildingManager::buildingCreated(const BWAPI::Unit* u);
 	void BuildingManager::executeOrders();
 	void BuildingManager::setIsDesiredToTrainWorkers(bool buildWorkers);
-	void BuildingManager::setIsDesiredToTrainMarines(bool trainWorkers);
-	void BuildingManager::setIsDesiredToTrainVultures(bool trainVultures);
+	BWAPI::TilePosition BuildingManager::spiralSearch();
 	bool expandFactory = true;
 	std::list<BWAPI::TechType> desiredResearchs;
 	std::list<BWAPI::UpgradeType> desiredUpgrades;
 	bool isDesiredToTrainWorkers = false;
-	bool isDesiredToTrainMarines = false;
-	bool isDesiredToTrainVultures = true;
-	bool isDesiredToTrainWraiths = false; 
 	const BWAPI::Unit* commandCenter;
+	BWAPI::UnitType factoryBuild;
+	BWAPI::UnitType barrackBuild;
 
 };
--- ExampleAIModule/source/ConstructionManager.cpp
+++ ExampleAIModule/source/ConstructionManager.cpp
@@ -13,26 +13,27 @@
 */
 
 std::list<int> startTime;
-
 using namespace BWAPI;
 using namespace Filter;
 
-BWAPI::UnitType orderedBuilding;
-std::list<TimedUnit*> builders;
+BWAPI::UnitType orderedBuilding; //Type of building to be built next.
+std::list<TimedUnit*> builders; //List of workers in the process of constructing buildings.
 
-//void ConstructionManager::setManagers(BuildingManager* b, UnitManager* u) {
-//	buildingManager =b;
-//    //unitManager = u;
-//}
-
+/**
+* Returns workers to gatheringManager if construction is complete.
+* If worker is idle, construct requested building.
+* This assures that building is constructed, even though worker might be obstructed.
+* @author Daniel Fjordhøj <s133198@dstudent.dtu.dk>
+*/
 void ConstructionManager::executeOrders() {
 
+	//Loops through all builders. If builder is idle, and a certain time has passed
+	//it is assigned back to the gatheringManager.
 	for (auto &b : builders) {
-
 		if (b->unit == NULL) {
 			builders.remove(b);
 			continue;
-			Broodwar->sendText("Null");
+			Broodwar->sendText("Null unit: code 20");
 		}
 		if (b->isOverTime(500) && b->isUnitIdle()) {
 			unitManager->newWorker(b->unit);
@@ -41,50 +42,65 @@ void ConstructionManager::executeOrders() {
 		}
 	}
 
+	/* Issues order to idle worker to construct building.
+	If construction is a refinery, special order is íssued.
+	*/
 
-	//Hvis vi har kontrol over en worker, og han ikke laver noget, så sættes han igang med at bygge bygningen specificeret i orderedBuilding
 	if (NULL != (constructionsWorker) && (*constructionsWorker)->isIdle()) {
 
 		if (orderedBuilding == UnitTypes::Terran_Refinery) {
 			buildRefinery(constructionsWorker);
 		}
-		else if (orderedBuilding == UnitTypes::Terran_Machine_Shop) {
-			buildingManager->expandFactory = true;
-		}
-		else
-		{
+		else {
 			TilePosition targetBuildLocation = Broodwar->getBuildLocation(orderedBuilding, (*constructionsWorker)->getTilePosition());
 			(*constructionsWorker)->build(orderedBuilding, targetBuildLocation);
-
 		}
 	}
-	
 }
 
+/**
+* Used bu unitManager to request the construction of a building
+* @param building UnitType type of building to construct
+* @param worker Unit worker assigned to this building
+* @author Daniel Fjordhøj <s133198@dstudent.dtu.dk>
+*/
 void ConstructionManager::createBuilding(BWAPI::UnitType building, const BWAPI::Unit* worker) {
-	//Find et sted hvor bygningen kan placeres, og sæt worker til at bygge den.
+	/*Order received from unitManager.
+	Saves building and worker, and issues order to worker to begin construction at available location. */
 
-	constructionsWorker = worker;
-	(*worker)->stop();
-	orderedBuilding = (building);
-	TilePosition targetBuildLocation = Broodwar->getBuildLocation(building, (*worker)->getTilePosition());
-	(*worker)->build(building, targetBuildLocation);
+	//Save order params, and stops worker action
 
+	if (NULL != worker) {
+		constructionsWorker = worker;
+		(*worker)->stop();
+		TimedUnit* t = new TimedUnit(worker, Broodwar->getFrameCount());
+		builders.push_back(t);
+	}
 
-	TimedUnit* t = new TimedUnit(worker, Broodwar->getFrameCount());
-	builders.push_back(t);
-
+	//Passing of extensions to buildingManager
+	if (building == UnitTypes::Terran_Machine_Shop) {
+		buildingManager->expandFactory = true;
+	}
+	else orderedBuilding = (building);
 }
 
 void ConstructionManager::expandBase() {
 
 }
 
+/**
+* Used to construct a refinery, which only can exist at a geyser node
+* @param worker Unit worker assigned to this building
+* @author Daniel Fjordhøj <s133198@dstudent.dtu.dk>
+*/
 void ConstructionManager::buildRefinery(const BWAPI::Unit* worker) {
+	//Find location of nearest geyser, and gets worker to build refinery on top of it.
 
+	//Initializing variable, and sets distance to be virtually unllimited units away.
 	BWAPI::Unit* gasLocation = new Unit();
 	int distance = 10000;
 
+	//Loop through all geysers, and if distance is smaller than last target, set this as new build target.
 	for (auto &u : Broodwar->getGeysers())
 	{
 		if ((*worker)->getDistance(u) < distance) {
@@ -93,12 +109,10 @@ void ConstructionManager::buildRefinery(const BWAPI::Unit* worker) {
 		}
 	}
 
+	//If geyser is found, construct at location
 	if (distance != 10000) {
-
-		// Broodwar->sendText("Refinery");
-
+		Broodwar->sendText("Refinery found");
 		BWAPI::Position pos = (*gasLocation)->getPosition();
-
 		TilePosition targetBuildLocation = Broodwar->getBuildLocation(UnitTypes::Terran_Refinery, (*gasLocation)->getTilePosition());
 		(*worker)->build(UnitTypes::Terran_Refinery, targetBuildLocation);
 	}
