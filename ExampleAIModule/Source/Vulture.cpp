727822ec1b51edc9248a87bd088963b71eb3e4f7
s164394 <s164394@student.dtu.dk>
2018-06-01 14:45:37 +0200

    yo

--- ExampleAIModule/Source/Vulture.cpp
+++ ExampleAIModule/Source/Vulture.cpp
@@ -2,6 +2,7 @@
 using namespace BWAPI;
 using namespace Filter;
 
+int counter = 0; 
 Vulture::Vulture(const BWAPI::Unit* u,  BWAPI::Position basePosition) {
 	unit = u;
 	time = Broodwar->getFrameCount(); 
@@ -20,9 +21,11 @@ void Vulture::putDownMineIfOutsideOfBase() {
 
 		BWAPI::Position position = (*unit)->getPosition() + Position(1, 1);
 
-		if (basePos.getDistance((*unit)->getPosition()) > 800.0) {
+		if (basePos.getDistance((*unit)->getPosition()) > 700.0 + counter) {
 			(*unit)->useTech(BWAPI::TechTypes::Spider_Mines, position);
-
+			counter = counter + 30.0; 
+			if (counter > 4000.0)
+				counter = 0; 
 		}
 	}
 }
