44e0e42eb5be8e7b1a191b332464857035b5bfe7
s164394 <s164394@student.dtu.dk>
2018-06-01 15:32:22 +0200

    yo

--- ExampleAIModule/Source/Vulture.cpp
+++ ExampleAIModule/Source/Vulture.cpp
@@ -4,9 +4,15 @@ using namespace Filter;
 
 int counter = 0; 
 Vulture::Vulture(const BWAPI::Unit* u,  BWAPI::Position basePosition) {
-	unit = u;
-	time = Broodwar->getFrameCount(); 
-	basePos = basePosition; 
+	if ((*u)->getType() == UnitTypes::Terran_Vulture) {
+		unit = u;
+		time = Broodwar->getFrameCount();
+		basePos = basePosition;
+
+	}
+	else {
+		Broodwar->sendText("ERROR! TRIED TO ADD NON-VULTURE UNIT TO VULTURE OBJECT!");
+	}
 }
 
 Vulture::~Vulture()
@@ -22,15 +28,15 @@ void Vulture::putDownMineIfOutsideOfBase() {
 		BWAPI::Position position = (*unit)->getPosition() + Position(1, 1);
 
 		if (basePos.getDistance((*unit)->getPosition()) > 700.0 + counter) {
-			(*unit)->useTech(BWAPI::TechTypes::Spider_Mines, position);
-			counter = counter + 30.0; 
+			(*unit)->useTech(BWAPI::TechTypes::Spider_Mines, position); 
+			counter = counter + 30.0;
 			if (counter > 4000.0)
-				counter = 0; 
+				counter = 0;
+			
 		}
 	}
 }
 
 bool Vulture::isUnitIdle() {
 	return (*unit)->isIdle();
-
 }
